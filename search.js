3b6f0a13e2c3bf4a798b7bec2b7d768a8bef9306
Andy Lau <andalau@gmail.com>
2014-10-22 14:50:18 -0700

    Update search.js

--- search.js
+++ search.js
@@ -4,7 +4,7 @@
 // Helper function to display JavaScript value on HTML page.
 function showResponse(response) {
 
-    var responseString = JSON.stringify(response, '', 3);
+    var responseString = JSON.stringify(response, '', 2);
     document.getElementById('response').innerHTML += responseString;
 }
 
