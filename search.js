0c2ef9ab2029ede76e115929c270f3342e76aea2
Andy Lau <andalau@gmail.com>
2014-10-22 16:37:40 -0700

    Update search.js

--- search.js
+++ search.js
@@ -55,7 +55,7 @@ function onYouTubeApiLoad() {
 
 function search() {
     var search_term = "";
-    search_term = document.getElementById("search_query");
+    search_term = document.getElementById("search_query").submit();
     console.log(search_term);
     
     // Use the JavaScript client library to create a search.list() API call.
