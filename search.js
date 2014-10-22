cc3489176c4572998e715ca26c48894181927119
Andy Lau <andalau@gmail.com>
2014-10-22 16:54:03 -0700

    Update search.js

--- search.js
+++ search.js
@@ -1,4 +1,9 @@
-var query = document.getElementById("query").value;
+var query = "";
+
+function queryValue() {
+    query = document.getElementById("query").value;
+}
+
 
 // Your use of the YouTube API must comply with the Terms of Service:
 // https://developers.google.com/youtube/terms
