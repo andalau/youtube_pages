9cf2b32a965dc0a79ab1834f1598692e8c18b127
Andy Lau <andalau@gmail.com>
2014-10-22 15:39:43 -0700

    Update search.js

--- search.js
+++ search.js
@@ -4,21 +4,24 @@
 // Helper function to display JavaScript value on HTML page.
 function showResponse(response) {
     console.log(response);
-    console.log(jsonobj);
     
     
     var responseString = JSON.stringify(response, '', 2);
     
+    //parsing json object
     var jsonobj = JSON.parse(responseString);
     
+    //collecting videoId links and placing them into an array
     var links = new Array()
     for (i = 0; i < 5; i++)
     {
         links[i] = jsonobj.items[i].id.videoId;
     }
     
-    
-    document.getElementById('response').innerHTML += links;
+    for (j = 0; j < 5; j++)
+    {
+        document.getElementById('response').innerHTML += jsonobj.items[j].snippet.title;
+    }
     
     
     
